0e6eb996ef78a81c3799da8600dd7ff45907c359
vasilaAliyeva <vasilaaliyeffa@gmail.com>
2026-04-02 15:53:56 +0400

    Fixing the service and their implementation

--- src/test/java/az/edu/ada/wm2/repository/ProductRepositoryTest.java
+++ src/test/java/az/edu/ada/wm2/repository/ProductRepositoryTest.java
@@ -10,7 +10,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 class ProductRepositoryTest {
